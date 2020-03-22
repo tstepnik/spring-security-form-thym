0b36d7a99dbef7002c9ff69faf4ca23f4633d756
tstepnik <tomasz.stepnik77@gmail.com>
2020-03-22 17:48:28 +0100

    essential files

---
+++ src/test/java/pl/javastart/SpringSecurityFormThymApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFormThymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
